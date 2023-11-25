b827ebcb97288b6bc843c77dca3f85a5885ecd07
TamannaTahsin1 <tamannatahsin648@gmail.com>
2023-11-26 00:20:46 +0600

    react helmet implimented

--- src/Pages/AllClasses/AllClasses.jsx
+++ src/Pages/AllClasses/AllClasses.jsx
@@ -1,12 +1,15 @@
 import { useLoaderData } from "react-router-dom";
 import AllClassesCard from "./AllClassesCard";
-
+import { Helmet } from 'react-helmet-async';
 
 const AllClasses = () => {
     const allClasses = useLoaderData()
     console.log(allClasses)
     return (
         <div>
+           <Helmet>
+        <title>Tutorio | Classes</title>
+      </Helmet>
                   <h1 className='text-center text-4xl mt-12 mb-20 font-extrabold bg-gradient-to-r from-blue-600 via-blue-300 to-gray-300 text-transparent bg-clip-text'>All Classes </h1>
                   <div
         className='container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,6 @@
+/** @format */
+
+import { Helmet } from "react-helmet";
 import Banner from "../../Components/Header/Banner";
 import Classes from "../Classes/Classes";
 import CheckOut from "./CheckOut/CheckOut";
@@ -5,15 +8,18 @@ import CheckOut from "./CheckOut/CheckOut";
 import Partners from "./Partners/Partners";
 
 const Home = () => {
-    return (
-        <div>
-           <Banner></Banner>
-           <Classes></Classes> 
-           <Partners></Partners>
-           <CheckOut></CheckOut>
-           {/* <Footer></Footer> */}
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Tutorio | Home</title>
+      </Helmet>
+      <Banner></Banner>
+      <Classes></Classes>
+      <Partners></Partners>
+      <CheckOut></CheckOut>
+      {/* <Footer></Footer> */}
+    </div>
+  );
 };
 
 export default Home;
