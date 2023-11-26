3dc214212cfc5fb37bcbca48f6083e1895873e36
TamannaTahsin1 <tamannatahsin648@gmail.com>
2023-11-27 00:31:28 +0600

    admin has been created

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -7,17 +7,31 @@ import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: users = [], refetch} = useQuery({
+  const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get("/users");
       return res.data;
     },
   });
-//   to make admin
- const handleMakeAdmin
-//   for delete 
- const handleDeleteUser = user =>{
+  //   to make admin
+  const handleMakeAdmin = (user) => {
+    axiosSecure.patch(`/users/admin/${user._id}`).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        refetch()
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.name} is an Admin now!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
+  //   for delete
+  const handleDeleteUser = (user) => {
     console.log(user);
     Swal.fire({
       title: "Are you sure?",
@@ -41,7 +55,7 @@ const AllUsers = () => {
         });
       }
     });
- }
+  };
   return (
     <div>
       <div className='flex justify-evenly my-4'>
@@ -63,23 +77,27 @@ const AllUsers = () => {
           </thead>
           <tbody>
             {/* row 1 */}
-            {users?.map((user,index) => (
+            {users?.map((user, index) => (
               <tr key={user._id}>
-                <th>{index+1}</th>
+                <th>{index + 1}</th>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
-                <td> 
+                <td>
+                  { user.role === 'admin' ? 'Admin' :
                     <button
-                    onClick={() => handleMakeAdmin(user)}
-                    className='btn btn-ghost btn-lg text-yellow-600'>
-                    <FaUsers />
-                  </button></td>
-                <td> 
-                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      className='btn btn-ghost btn-lg text-yellow-600'>
+                      <FaUsers />
+                    </button>
+                  }
+                </td>
+                <td>
+                  <button
                     onClick={() => handleDeleteUser(user)}
                     className='btn btn-ghost btn-sm text-red-600'>
                     <FaTrashAlt />
-                  </button></td>
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
